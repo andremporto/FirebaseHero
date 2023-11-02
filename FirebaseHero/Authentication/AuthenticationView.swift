2a8572ad8289ed84f3141d1a1153971362b282a8
André Porto <andreporto@me.com>
2023-11-02 10:59:46 -0300

    Connect Firebase to Xcode

---
+++ FirebaseHero/Authentication/AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  FirebaseHero
+//
+//  Created by André Porto on 02/11/23.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
