75cce85be04842457e718019994a7ded582250db
André Porto <andreporto@me.com>
2024-04-02 08:53:11 -0300

    Initial Commit

---
+++ FirebaseHero/FirebaseHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseHeroApp.swift
+//  FirebaseHero
+//
+//  Created by André Porto on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
